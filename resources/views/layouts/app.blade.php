829a9155aeaa05666265347c6a0c95c81c1175e4
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2020-12-07 12:32:52 +0000

    Created new views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,8 +33,8 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-                <b-nav-item href="#">Link</b-nav-item>
-                <b-nav-item href="#">Link 2</b-nav-item>
+                <b-nav-item href="/variedades">Variedades</b-nav-item>
+                <b-nav-item href="/agronomic_data">Datos Agronómicos</b-nav-item>
             </b-navbar-nav>
         </b-collapse>
         </b-navbar>
---
+++ resources/views/variedades.blade.php
@@ -0,0 +1,15 @@
+
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+         <!-- start here -->
+          variedades 
+           
+        </div>
+    </div>
+</div>
+
+@endsection
