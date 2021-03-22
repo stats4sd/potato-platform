0258a9ebc0da68b8222f7f61d6aff30389a1e3d8
Dave Mills <d.e.mills@stats4sd.org>
2021-03-22 19:36:14 +0000

    tweaks to parameter column names and values:  - avoiding key-clash in arrays,  - aligning values with db column names

--- app/Http/Controllers/Api/ParameterFilterController.php
+++ app/Http/Controllers/Api/ParameterFilterController.php
@@ -14,12 +14,58 @@ use Illuminate\Support\Facades\DB;
 class ParameterFilterController extends Controller
 {
     public function index()
-    { 
-        //Flowering
-        $columns_flowering = array(
-            'Habito de crecimiento de la planta'=>'plant_growth', 'Tipo de la disección de la hoja'=>'leaf_dissection', 
-            'Número de foliolos laterales de la hoja'=>'number_lateral_leaflets', 
-            'Número de inter-hojuelas entre foliolos laterales'=>'number_intermediate_leaflets', 
+    {
+        $model_flowering = 'App\\Models\\Flowering';
+        $array_flowering = $this->generateArrayOptions($this->getColumnsFlowering(), $model_flowering);
+        $options['Floración'] =  $array_flowering;
+
+        $model_fructification = 'App\\Models\\Fructification';
+        $array_fructification = $this->generateArrayOptions($this->getColumnsFructification(), $model_fructification);
+        $options['Fructificación'] =  $array_fructification;
+
+        $model_tubers_at_harvest = 'App\\Models\\TubersAtHarvest';
+        $array_tubers_at_harvest = $this->generateArrayOptions($this->getColumnsTubersAtHarvest(), $model_tubers_at_harvest);
+        $options['Tubérculos a la Cosecha'] =  $array_tubers_at_harvest;
+
+        $model_sprouts = 'App\\Models\\Sprout';
+        $array_sprouts = $this->generateArrayOptions($this->getColumnsSprouts(), $model_sprouts);
+        $options['Brotamiento'] =  $array_sprouts;
+
+        return $options;
+    }
+
+    public function generateArrayOptions(array $columns, $model)
+    {
+        $data=[];
+        $choices_all = Choice::all();
+        $model_all = $model::all();
+
+        foreach ($columns as $key_column => $column) {
+            $flowering =   $model_all->pluck($column)->unique();
+            $column_data['label']= $key_column;
+            $column_data['value']= $column;
+            $column_data['options'] = $flowering;
+
+            foreach ($column_data['options'] as $key => $value) {
+                $choice =  $choices_all->where('id', $value)->first();
+                if ($choice) {
+                    $column_data['options'][$key]= $choice->label_spanish;
+                } else {
+                    $column_data['options'][$key]= $value;
+                }
+            }
+            $data[] = $column_data;
+        }
+
+        return $data;
+    }
+
+    public static function getColumnsFlowering()
+    {
+        return array(
+            'Habito de crecimiento de la planta'=>'plant_growth', 'Tipo de la disección de la hoja'=>'leaf_dissection',
+            'Número de foliolos laterales de la hoja'=>'number_lateral_leaflets',
+            'Número de inter-hojuelas entre foliolos laterales'=>'number_intermediate_leaflets',
             'Número de inter-hojuelas sobre peciolulos'=>'number_leaflets_on_petioles',
             'Color de tallo de esta planta'=>'color_stem',
             'Forma de las alas del tallo' => 'shape_stem_wings',
@@ -38,82 +84,44 @@ class ParameterFilterController extends Controller
             'Nivel de tolerancia a la helada' => 'level_tolerance_frost',
             'Nivel de tolerancia a la sequía' => 'level_tolerance_drought'
         );
+    }
 
-        //Fructification
-        $columns_fructification= array(
+    public static function getColumnsFructification()
+    {
+        return array(
             'Color de las baya' => 'color_berries',
             'Forma de la baya' => 'shape_berry',
             'La madurez' => 'maturity_variety'
         );
+    }
 
-        //TubersAtHarvest
-        $columns_tubers_at_harvest= array(
+    public static function getColumnsTubersAtHarvest()
+    {
+        return array(
             'Color predominante' => 'color_predominant_tuber',
             'Intensidad del color predominante' => 'intensity_color_predominant_tuber',
-            'Color secundario' => 'color_secondary_tuber',
+            'Color secundario' => 'color_secodary_tuber',
             'Distribución del color secundario' => 'distribution_color_secodary_tuber',
             'Forma general' => 'shape_tuber',
             'Variante de forma' => 'variant_shape_tuber',
             'Profundidad de los ojos' => 'depth_tuber_eyes',
             'Color predominante de la pulpa' => 'color_predominant_tuber_pulp',
-            'Color secundario' => 'color_secondary_tuber_pulp',
-            'Distribución del color secundario' => 'distribution_color_secodary_tuber_pulp',
+            'Color secundario de la pulpa' => 'color_secondary_tuber_pulp',
+            'Distribución del color secundario de la pulpa' => 'distribution_color_secodary_tuber_pulp',
             'Nivel de tolerancia a la rancha' => 'level_tolerance_late_blight',
             'Nivel de tolerancia al gorgojo de los andes' => 'level_tolerance_weevil',
             'Nivel de tolerancia a la granizada' => 'level_tolerance_hailstorms',
             'Nivel de tolerancia a la helada' => 'level_tolerance_frost',
             'Nivel de tolerancia a la sequía' => 'level_tolerance_drought'
         );
-        //Sprouts
-        $columns_sprouts= array(
+    }
+
+    public static function getColumnsSprouts()
+    {
+        return array(
             'Color predominante' => 'color_predominant_tuber_shoot',
             'Color secundario' => 'color_secondary_tuber_shoot',
             'Distribución del color secundario' => 'distribution_color_secodary_tuber_shoot',
         );
-        
-        $model_flowering = 'App\\Models\\Flowering';
-        $array_flowering = $this->generateArrayOptions($columns_flowering, $model_flowering);
-        $options['Floración'] =  $array_flowering;
-
-        $model_fructification = 'App\\Models\\Fructification';
-        $array_fructification = $this->generateArrayOptions($columns_fructification, $model_fructification);
-        $options['Fructificación'] =  $array_fructification;
-
-        $model_tubers_at_harvest = 'App\\Models\\TubersAtHarvest'; 
-        $array_tubers_at_harvest = $this->generateArrayOptions($columns_tubers_at_harvest, $model_tubers_at_harvest);
-        $options['Tubérculos a la Cosecha'] =  $array_tubers_at_harvest;
-
-        $model_sprouts = 'App\\Models\\Sprout';
-        $array_sprouts = $this->generateArrayOptions($columns_sprouts, $model_sprouts);
-        $options['Brotamiento'] =  $array_sprouts;
-
-        return $options;
-        
-    }
-
-    public function generateArrayOptions(Array $columns, $model)
-    {
-        $data=[];
-        $choices_all = Choice::all();
-        $model_all = $model::all();
-
-        foreach ($columns as $key_column => $column) {
-            $flowering =   $model_all->pluck($column)->unique();
-            $column_data['label']= $key_column;
-            $column_data['value']= $column;
-            $column_data['options'] = $flowering;
-        
-            foreach ($column_data['options'] as $key => $value) {
-                $choice =  $choices_all->where('id', $value)->first();
-                if($choice){
-                    $column_data['options'][$key]= $choice->label_spanish;
-                } else {
-                    $column_data['options'][$key]= $value;
-                }
-            }
-            $data[] = $column_data;
-        }
-
-        return $data;
     }
 }
